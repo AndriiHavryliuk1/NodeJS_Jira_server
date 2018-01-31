46240ec2ce1f284db7b6c613a67f8c27a3d24006
AndriiHavryliuk <andriy.gavrulyik@gmail.com>
2018-02-01 00:43:55 +0200

    improved task controller

--- api/controllers/tasksController.js
+++ api/controllers/tasksController.js
@@ -1,13 +1,28 @@
 const Task = require('../models/task');
+const mongoose = require('mongoose');
 
 exports.get_all_tasks = (req, res, next) => {
     Task.find().exec()
         .then(result => {
+
             res.status(200).json(result);
         })
         .catch(error => res.status(500).json({
             message: error.message
         }));
+
+        async function getByIds(chield_ids) {
+        
+            model.find({
+                '_id': { $in: [
+                    mongoose.Types.ObjectId('4ed3ede8844f0f351100000c'),
+                    mongoose.Types.ObjectId('4ed3f117a844e0471100000d'), 
+                    mongoose.Types.ObjectId('4ed3f18132f50c491100000e')
+                ]}
+            }, function(err, docs){
+                 console.log(docs);
+            });
+        }
 };
 
 
@@ -32,27 +47,37 @@ exports.get_task_by_id = (req, res, next) => {
 
 
 exports.create_task = (req, res, next) => {
-    const task = new Task({
-        _id: mongoose.Types.ObjectId(),
-        task_id: 1,
-        name: req.body.name,
-        description: req.body.description,
-        parent_id: req.body.parent_id,
-        children_ids: req.body.children_ids,
-        user_id: req.body.user_id,
-        column_id: req.body.column_id,
-        status: req.body.status
-    })
+    Task.find().sort({ task_id: -1 }).limit(1)
+        .then(taskWithMaxId => {
+            console.log(taskWithMaxId);
+            createTask(taskWithMaxId)
+        }).catch(error => {
+            createTask(error);
+        });
 
-    task.save().then(result => {
-        res.status(200).json({
-            result: result
+    function createTask(taskWithMaxId) {
+        const task = new Task({
+            _id: mongoose.Types.ObjectId(),
+            task_id: ++taskWithMaxId.task_id || 1,
+            name: req.body.name,
+            description: req.body.description,
+            parent_id: req.body.parent_id,
+            children_ids: req.body.children_ids,
+            user_id: req.body.user_id,
+            column_id: req.body.column_id,
+            status: req.body.status
         })
-    }).catch(error => {
-        res.status(500).json({
-            error: error
-        })
-    });
+
+        task.save().then(result => {
+            res.status(200).json({
+                result: result
+            })
+        }).catch(error => {
+            res.status(500).json({
+                error: error
+            })
+        });
+    }
 };
 
 exports.update_task = (req, res, next) => {
